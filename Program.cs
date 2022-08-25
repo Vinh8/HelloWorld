545843b4419d92eb58c077ce1e4c68645e22ede2
myste <myste@VINH>
2022-08-25 16:29:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string stg = "Hello World";
+            Console.WriteLine(stg);
+            Console.WriteLine("Enter Username");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter user's age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Username is "+ userName +" " + "Age is " + age);
+        }
+    }
+}
